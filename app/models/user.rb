3d85ccf7f25813c333c52835f644e238dd77a36a
Elijahscriptdev <elijahobominuru@gmail.com>
2020-01-15 11:23:53 +0100

    fixed lint errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,9 @@ class User < ApplicationRecord
   # validations
   validates :user, presence: true, length: { minimum: 3 }
 
-  REGEX_VALID = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+  REG = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
-  validates :email, presence: true, length: { maximum: 255 },
-            format: { with: REGEX_VALID }, uniqueness: { case_sensitive: true }
+  validates :email, presence: true, length: { maximum: 99 }, format: { with: REG }, uniqueness: { case_sensitive: true }
 
   # associations
   has_many :posts
